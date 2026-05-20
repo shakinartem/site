8cccda13d42b51c503e4e321f268a11154219011
shakinartem <sakinartem28@gmail.com>
2026-05-20 11:22:56 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const githubPagesBase = '/site/';
-
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === 'serve' ? '/' : githubPagesBase,
-}));
+  base: '/',
+});
